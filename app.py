c90e0543898caa29114554467203424921e95531
Eko <swdev.bali@gmail.com>
2019-11-03 23:35:05 +0700

    solved running flask and docker on separate docker-compose

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-API_URL = 'http://localhost:8000/api/v1'
+API_URL = 'http://djangoapp:8000/api/v1'
 
 @app.route('/')
 def index():
+    roles = {}
     roles = requests.get(f'{API_URL}/roles/')
+    print(roles)
     roles = roles.json()
 
     return render_template('index.html', roles=roles)
